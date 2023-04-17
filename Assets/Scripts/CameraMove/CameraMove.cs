2782e2b12768626f250d3b7387f1692508e03d4f
ShadowChris <cai.zi.mo@163.com>
2023-04-17 18:20:49 +0800

    设置相机移动最大距离
    待测试

--- Assets/Scripts/CameraMove/CameraMove.cs
+++ Assets/Scripts/CameraMove/CameraMove.cs
@@ -1,28 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraMove : MonoBehaviour
 {
-    public Transform handTransform; // 您的手部追踪数据来源，例如手部模型的 Transform 组件
-    public Transform cameraTransform; // 摄像头的 Transform 组件
-    public float moveSpeed = 1.0f; // 摄像头移动速度
-    public float boundaryThreshold = 0.8f; // 边界阈值，范围 0-1，例如 0.8 表示手部位置到达屏幕宽度的 80% 时触发移动
+    public Transform handTransform;
+    public Transform cameraTransform;
+    public Transform characterTransform;
+    public float moveSpeed = 1.0f;
+    public float boundaryThreshold = 0.8f;
+    public Vector2 horizontalBoundary = new Vector2(-10, 10); // 水平移动范围
+    public Vector2 verticalBoundary = new Vector2(-10, 10); // 垂直移动范围
+    public bool isEnabled = true; // 控制脚本是否启用
 
     private Camera _camera;
     private float _screenWidth;
     private float _screenHeight;
+    private Vector3 _initialCharacterPosition; // 角色的初始位置
 
     void Start()
     {
         _camera = cameraTransform.GetComponent<Camera>();
         _screenWidth = Screen.width;
         _screenHeight = Screen.height;
+        _initialCharacterPosition = characterTransform.position;
     }
 
     void Update()
     {
+        if (!isEnabled) return; // 如果脚本未启用，则直接返回，不执行后续代码
+
         Vector3 screenPos = _camera.WorldToScreenPoint(handTransform.position);
+
+        Vector3 cameraMoveDirection = Vector3.zero;
 
         // 检查手部是否靠近屏幕左右边界
         if (screenPos.x < _screenWidth * (1 - boundaryThreshold) || screenPos.x > _screenWidth * boundaryThreshold)
@@ -30,8 +38,8 @@ public class CameraMove : MonoBehaviour
             // 计算摄像头移动的方向
             float moveDirectionX = screenPos.x < _screenWidth * (1 - boundaryThreshold) ? -1 : 1;
 
-            // 移动摄像头
-            cameraTransform.position += cameraTransform.right * moveSpeed * moveDirectionX * Time.deltaTime;
+            // 计算摄像头移动向量
+            cameraMoveDirection += cameraTransform.right * moveSpeed * moveDirectionX * Time.deltaTime;
         }
 
         // 检查手部是否靠近屏幕上下边界
@@ -40,9 +48,20 @@ public class CameraMove : MonoBehaviour
             // 计算摄像头移动的方向
             float moveDirectionY = screenPos.y < _screenHeight * (1 - boundaryThreshold) ? -1 : 1;
 
-            // 移动摄像头
-            cameraTransform.position += cameraTransform.up * moveSpeed * moveDirectionY * Time.deltaTime;
-
+            // 计算摄像头移动向量
+            cameraMoveDirection += cameraTransform.up * moveSpeed * moveDirectionY * Time.deltaTime;
         }
+
+        // 更新角色位置
+        Vector3 newCharacterPosition = characterTransform.position + cameraMoveDirection;
+
+        // 限制角色在边界内移动
+        newCharacterPosition.x = Mathf.Clamp(newCharacterPosition.x, _initialCharacterPosition.x + horizontalBoundary.x, _initialCharacterPosition.x + horizontalBoundary.y);
+        newCharacterPosition.y = Mathf.Clamp(newCharacterPosition.y, _initialCharacterPosition.y + verticalBoundary.x, _initialCharacterPosition.y + verticalBoundary.y);
+
+        // 更新摄像头和角色的位置
+        Vector3 deltaPosition = newCharacterPosition - characterTransform.position;
+        cameraTransform.position += deltaPosition;
+        characterTransform.position = newCharacterPosition;
     }
 }
