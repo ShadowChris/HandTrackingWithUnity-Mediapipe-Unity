5d8f7bb6ba1d8f9aeec3773351bce30f7b36a6ab
ShadowChris <cai.zi.mo@163.com>
2023-05-15 23:44:31 +0800

    小球击中标靶记分

---
+++ Assets/Scripts/Targets/ScoreCounter.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.UI;
+public class ScoreCounter : MonoBehaviour
+{
+    private int score = 0;
+
+    public Text ScoreText;
+
+    private void Start()
+    {
+        ScoreText.text = score.ToString();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Ball"))
+        {
+            score++;
+            ScoreText.text = score.ToString();
+        }
+    }
+}
