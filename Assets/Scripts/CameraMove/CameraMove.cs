2646989c4f2817a1b263a9ad8470b23bf41ac4c1
ShadowChris <cai.zi.mo@163.com>
2023-05-21 00:10:40 +0800

    调整摄像头移动逻辑

--- Assets/Scripts/CameraMove/CameraMove.cs
+++ Assets/Scripts/CameraMove/CameraMove.cs
@@ -1,3 +1,4 @@
+using landmarktest;
 using UnityEngine;
 
 public class CameraMove : MonoBehaviour
@@ -5,16 +6,17 @@ public class CameraMove : MonoBehaviour
     public Transform handTransform;
     public Transform cameraTransform;
     public Transform characterTransform;
+    public HandTracking handTracking;
     public float moveSpeed = 1.0f;
     public float boundaryThreshold = 0.8f;
-    public Vector2 horizontalBoundary = new Vector2(-10, 10); // 水平移动范围
-    public Vector2 verticalBoundary = new Vector2(-10, 10); // 垂直移动范围
-    public bool isEnabled = true; // 控制脚本是否启用
+    public Vector2 horizontalBoundary = new Vector2(-10, 10);
+    public Vector2 verticalBoundary = new Vector2(-10, 10);
+    public bool isEnabled = true;
 
     private Camera _camera;
     private float _screenWidth;
     private float _screenHeight;
-    private Vector3 _initialCharacterPosition; // 角色的初始位置
+    private Vector3 _initialCharacterPosition;
 
     void Start()
     {
@@ -26,40 +28,44 @@ public class CameraMove : MonoBehaviour
 
     void Update()
     {
-        if (!isEnabled) return; // 如果脚本未启用，则直接返回，不执行后续代码
+        if (!isEnabled) return;
 
         Vector3 screenPos = _camera.WorldToScreenPoint(handTransform.position);
 
         Vector3 cameraMoveDirection = Vector3.zero;
+        string gestureLabel = handTracking.gestureLabel;
 
-        // 检查手部是否靠近屏幕左右边界
-        if (screenPos.x < _screenWidth * (1 - boundaryThreshold) || screenPos.x > _screenWidth * boundaryThreshold)
+        // 如果手势标签是"left"并且手部靠近屏幕左边界
+        if (gestureLabel == "left" && screenPos.x < _screenWidth * (1 - boundaryThreshold))
         {
-            // 计算摄像头移动的方向
-            float moveDirectionX = screenPos.x < _screenWidth * (1 - boundaryThreshold) ? -1 : 1;
-
-            // 计算摄像头移动向量
-            cameraMoveDirection += cameraTransform.right * moveSpeed * moveDirectionX * Time.deltaTime;
+            cameraMoveDirection += cameraTransform.right * moveSpeed * -1 * Time.deltaTime;
         }
 
-        // 检查手部是否靠近屏幕上下边界
-        if (screenPos.y < _screenHeight * (1 - boundaryThreshold) || screenPos.y > _screenHeight * boundaryThreshold)
+        // 如果手势标签是"right"并且手部靠近屏幕右边界
+        if (gestureLabel == "right" && screenPos.x > _screenWidth * boundaryThreshold)
         {
-            // 计算摄像头移动的方向
-            float moveDirectionY = screenPos.y < _screenHeight * (1 - boundaryThreshold) ? -1 : 1;
-
-            // 计算摄像头移动向量
-            cameraMoveDirection += cameraTransform.up * moveSpeed * moveDirectionY * Time.deltaTime;
+            cameraMoveDirection += cameraTransform.right * moveSpeed * Time.deltaTime;
         }
 
-        // 更新角色位置
+        // 如果手势标签是"up"并且手部靠近屏幕上边界
+        if (gestureLabel == "up" && screenPos.y > _screenHeight * boundaryThreshold)
+        {
+            cameraMoveDirection += cameraTransform.up * moveSpeed * Time.deltaTime;
+        }
+
+        // 如果手势标签是"down"并且手部靠近屏幕下边界
+        if (gestureLabel == "down" && screenPos.y < _screenHeight * (1 - boundaryThreshold))
+        {
+            cameraMoveDirection += cameraTransform.up * moveSpeed * -1 * Time.deltaTime;
+        }
+
         Vector3 newCharacterPosition = characterTransform.position + cameraMoveDirection;
-
-        // 限制角色在边界内移动
         newCharacterPosition.x = Mathf.Clamp(newCharacterPosition.x, _initialCharacterPosition.x + horizontalBoundary.x, _initialCharacterPosition.x + horizontalBoundary.y);
         newCharacterPosition.y = Mathf.Clamp(newCharacterPosition.y, _initialCharacterPosition.y + verticalBoundary.x, _initialCharacterPosition.y + verticalBoundary.y);
 
-        // 更新摄像头和角色的位置
+        // Maintain the same z position
+        newCharacterPosition.z = _initialCharacterPosition.z;
+
         Vector3 deltaPosition = newCharacterPosition - characterTransform.position;
         cameraTransform.position += deltaPosition;
         characterTransform.position = newCharacterPosition;
